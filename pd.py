85f970961268b949909e190848a24453639c2cb8
John <a0982938209@gmail.com>
2023-02-12 20:22:05 +0800

    init

---
+++ pd.py
@@ -0,0 +1,11 @@
+import pandas as pd
+ 
+ 
+grades = {
+    "name": ["Mike", "Sherry", "Cindy", "John"],
+    "math": [80, 75, 93, 86],
+    "chinese": [63, 90, 85, 70]
+}
+ 
+df = pd.DataFrame(grades)
+print(df)
